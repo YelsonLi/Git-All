66adeecf8c06418d1c263a27a27f9c2454326ce4
yelsonli <767798711@qq.com>
2022-02-23 17:34:37 +0800

    git-2022-2-23-17:34-第一次

---
+++ src/gitall/GitFirst.java
@@ -0,0 +1,10 @@
+package gitall;/*
+ *    Create By Yelson Li on 2022/2/19.
+ *
+ */
+
+public class GitFirst {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
---
+++ src/gittest1/Wa.java
@@ -0,0 +1,7 @@
+package gittest1;/*
+ *    Create By Yelson Li on 2022/2/23.
+ *
+ */
+
+public class Wa {
+}
